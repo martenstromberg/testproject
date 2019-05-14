7a8e93f30e3de4fd7a0c2b6c4e928573fc2956d3
Marten Stromberg <marten.b.stromberg@gmail>
2019-05-14 07:52:33 +0200

    update master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Nytt test att committa</h2>
+      <h2>Updated master</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
