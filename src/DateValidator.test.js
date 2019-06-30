58078abe30a90ee4f50d934ed3d5d1dcb2a5982b
Marten Stromberg <marten.b.stromberg@gmail>
2019-06-30 12:40:25 +0200

    add next date

---
+++ src/DateValidator.test.js
@@ -0,0 +1,11 @@
+import ValidDateFormat from './DateValidator'
+
+it('has valid date', ()=> {
+    let input = "2018-12-31"
+    expect(ValidDateFormat(input)).toEqual(true)
+})
+
+it('has invalid date', ()=> {
+    let input = "2018-13-45"
+    expect(ValidDateFormat(input)).toEqual(false)
+})
