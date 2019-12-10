9a279cca5976a8380f7e2bfffc27130b26699bc9
wardag <dagwardaeus@hotmail.com>
2019-12-10 20:58:12 +0100

    visa lista och snyggare navbar

--- src/Attendants.js
+++ src/Attendants.js
@@ -6,17 +6,64 @@ class Attendants extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+
+      attendants: []
+    }
+}
+
+componentDidMount() {
+
+  this.doReadData()
+
+
 }
 
   readFromDB = () => {
       this.props.firebase.doReadData()
   }
 
+  doReadData = () => {
+      this.props.firebase.db.collection("dinner").get().then((querySnapshot) => {
+
+          querySnapshot.forEach((doc) => {
+              if (doc.id === "dinner1") {
+                this.setState({attendants: doc.data().attendants})
+              }
+              })
+            })
+        }
+
+  showNames = () => {
+
+     const list = ["dag", "mprten", "ohilipo"]
+
+              return (
+                  list.map((item) => (
+                    <li>{item} </li>
+                  ))
+              )
+            }
+
   render() {
 
+    this.showNames()
     return (
-     <div class="flex content-center flex-wrap bg-white-200 h-48">
 
+
+     <div class="flex content-center flex-wrap bg-white-200 h-48">
+     <p class="w-full p-2 text-center">
+     Vi som kommer:
+     </p>
+     <ol class="w-full p-2 text-center">
+      {
+      this.state.attendants.map( attendant => (
+        <li>{attendant}</li>
+        )
+      )
+      }
+      </ol>
      </div>
     );
   }
